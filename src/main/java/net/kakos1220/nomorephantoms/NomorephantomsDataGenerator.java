ef018c0ea1c8018f6aea428574055203ef800b7d
Körmendi Ákos <KAkosyyy@gmail.com>
2024-12-09 17:57:49 +0100

    Fixed bugs.

--- src/main/java/net/kakos1220/nomorephantoms/NomorephantomsDataGenerator.java
+++ src/main/java/net/kakos1220/nomorephantoms/NomorephantomsDataGenerator.java
@@ -2,10 +2,13 @@ package net.kakos1220.nomorephantoms;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.kakos1220.nomorephantoms.datagen.ModRecipeProvider;
 
 public class NomorephantomsDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(ModRecipeProvider::new);
 	}
 }
